8c3d34685b09a2c04c6669405ec6d5b6c2e15fa0
Andreas Billmann <andreas@billmann.de>
2021-03-10 14:45:28 +0100

    base64 implementation

---
+++ cmd/base64go/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+var fileFlag = flag.String("file", "", "file as input")
+var modeFlag = flag.String("mode", "encode", "encode or decode as mode")
+
+func main() {
+	flag.Parse()
+
+	input, err := input()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inputAsString := streamToString(input)
+
+	var output string
+	switch {
+	case *modeFlag == "encode":
+		output = encode(inputAsString)
+	case *modeFlag == "decode":
+		output = decode(inputAsString)
+	default:
+		log.Fatal("mode must be encode or decode")
+	}
+
+	fmt.Println(output)
+}
+
+func streamToString(stream io.Reader) string {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(stream)
+	return buf.String()
+}
+
+func input() (io.Reader, error) {
+	switch {
+	case *fileFlag != "":
+		f, err := os.Open(*fileFlag)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		return f, nil
+	case len(flag.Args()) == 1:
+		return strings.NewReader(flag.Args()[0]), nil
+	case len(flag.Args()) == 0:
+		return os.Stdin, nil
+	default:
+		return nil, errors.New("Provide a file or an argument or use the standard input!")
+	}
+}
+
+func encode(msg string) string {
+	return base64.StdEncoding.EncodeToString([]byte(msg))
+}
+
+func decode(msg string) string {
+	decoded, _ := base64.StdEncoding.DecodeString(msg)
+	return string(decoded)
+}
