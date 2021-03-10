8152b7762a5d12e1c7fa85cbeb4905c5445c1780
Andreas Billmann <andreas@billmann.de>
2021-03-10 07:39:24 +0100

    extend the inputs to standard in

--- cmd/md5go/main.go
+++ cmd/md5go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/md5"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -14,24 +15,29 @@ var fileFlag = flag.String("file", "", "file as input")
 
 func main() {
 	flag.Parse()
-	output := calcMD5(input())
+	input, err := input()
+	if err != nil {
+		log.Fatal(err)
+	}
+	output := calcMD5(input)
 	fmt.Printf("%x", output)
 }
 
-func input() io.Reader {
-	if *fileFlag != "" {
-		f, err := os.Open("file.txt")
+func input() (io.Reader, error) {
+	switch {
+	case *fileFlag != "":
+		f, err := os.Open(*fileFlag)
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer f.Close()
-		return f
-	} else {
-		args := os.Args
-		if len(args) != 1 {
-			fmt.Println(args[0])
-		}
-		return strings.NewReader(args[1])
+		return f, nil
+	case len(os.Args) == 2:
+		return strings.NewReader(os.Args[1]), nil
+	case len(os.Args) == 1:
+		return os.Stdin, nil
+	default:
+		return nil, errors.New("Provide a file or an argument or use the standard input!")
 	}
 }
 
